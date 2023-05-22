451a72d44075b3998e6aa349c34a251752dfdd30
Artem <smirnov@ddplanet.ru>
2023-05-22 12:28:03 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./index.scss";
 import { Success } from "./components/Success";
 import { Users } from "./components/Users";
 
-// Тут список пользователей: https://reqres.in/api/users
+//список пользователей: https://reqres.in/api/users
 
 function App() {
   const [users, setUsers] = React.useState([]);
