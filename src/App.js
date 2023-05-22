e0439e131599041ead631c5fbd8461f85a56fd11
Artem <smirnov@ddplanet.ru>
2023-05-22 16:17:52 +0300

    подключение api и создание проекта

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import './index.scss';
+import React from "react";
+import { Block } from "./Block";
+import "./index.scss";
+import { json } from "body-parser";
 
 function App() {
+  const [rates, setRates] = React.useState({});
+
+  React.useEffect(() => {
+    fetch("https://cdn.cur.su/api/latest.json")
+      .then((res) => res.json())
+      .then((json) => {
+        setRates(json.retes);
+        console.log(json.rates);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Не удалось получить информацию");
+      });
+  }, []);
   return (
     <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
-      </div>
+      <Block
+        value={0}
+        currency="RUB"
+        onChangeCurrency={(cur) => console.log(cur)}
+      />
+      <Block value={0} currency="USD" />
     </div>
   );
 }
